8d7e9c57e1f696b6c56ff0e1d2e5eb13aa83ee24
GrinIv <svetili@bk.ru>
2023-07-16 13:52:38 +0300

    task23

--- 3_sem/Program.cs
+++ 3_sem/Program.cs
@@ -1,12 +1,13 @@
 ﻿
 //1.
-/*
+
 //Напишите программу, которая на вход принимает пятизначное число и проверяет, является ли 
 //оно палиндромом первого числа
-*/
+
+
 
 //2.
-
+/*
 //Напишите прогшрамму , которая принимает на вход координаты двух точек и находит расстояние 
 //между ними в 3D пространстве 
 
@@ -33,8 +34,24 @@ int z2 = Convert.ToInt32(Console.ReadLine());
 
 double rezult = Distanse(x1, y1, z1, x2, y2, z2);
 Console.WriteLine(rezult);
+*/
+
 
 //3
 //Напишите программу, которая принимает на вход число (N) и выдает таблицу  кубов 
 // чисел от 1 до N
 
+//method void
+void TabKub (int number)
+{
+    int count =1;
+    while (count <= number)
+    {
+        Console.WriteLine($"{count} * {count} * {count} = {count*count*count}");
+        count++;        
+    }
+    
+}
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+TabKub(number);
