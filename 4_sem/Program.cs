51148a1f3db79659b873dd776f39b38bd9518da2
GrinIv <svetili@bk.ru>
2023-07-16 21:15:07 +0300

    task25

---
+++ 4_sem/Program.cs
@@ -0,0 +1,28 @@
+﻿
+//Task 25
+//Напишите программу, которая принимает на вход два числа (А и В) и возводит число А 
+//в натуральную степень В (цикл)
+
+int Degree(int a, int b)
+{
+int count = 1;
+int deg = 1;
+while(count<=b)
+{
+    deg = deg * a;
+    count++;
+}
+return deg;
+}
+Console.WriteLine("Введите число, которое нужно возвести в степень: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число, указывающее на желаемую степень: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Degree(num1, num2));
+
+//Task27
+//
+//Task29
+//
+
