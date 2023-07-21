5f9c831fce88968616aad8e6e5b172a6ec68e5a0
GrinIv <svetili@bk.ru>
2023-07-22 01:40:11 +0300

    task 29

--- 4_sem/Program.cs
+++ 4_sem/Program.cs
@@ -25,7 +25,7 @@ Console.WriteLine(Degree(num1, num2));
 
 //Task27
 //Напишите программу, которая принимает на вход число и выдает сумму цифр в числе
-
+/*
 int Sum(int number)
 {
     int ost = 0;
@@ -40,6 +40,37 @@ int Sum(int number)
 Console.WriteLine("Введите число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Сумма цифр введенного числа = " + Sum(num1));
-//Task29
-//
 
+*/
+
+//Task29
+//Напишите программу, которая задает массив из m элементов и выводит их на экран.
+//Разработать метод CreateArray(int size), генерирующий массив на основе данных, 
+//вводимых пользователем
+
+int[] CreateArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for(int i=0; i < size; i++)
+    {
+        Console.WriteLine("Введите следующий элемент массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+Console.Write("input length");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("input min value");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("input max value");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] myarray = CreateArray(length, min, max);
+ShowArray(myarray);
