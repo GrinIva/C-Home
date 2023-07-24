a8f6ae2257b4d305dad9998abb4c18387f11c187
GrinIv <svetili@bk.ru>
2023-07-25 01:14:39 +0300

    36 task

--- 5_sem/Program.cs
+++ 5_sem/Program.cs
@@ -1,6 +1,7 @@
 ﻿//Task 34. Задайте массив заполненный случайными трехзначными положительными числами.
 //напишите программу, которая покажет количество четных чисел в массиве
 
+/*
 int[] CreateArray(int size, int minValue, int maxValue)
 {
     int[] array = new int[size];
@@ -35,11 +36,44 @@ int[] myarray = CreateArray(length,min,max);
 ShowArray(myarray);
 
 Console.WriteLine("Массив содержит " + CountEvenNumber(myarray) + " четных элемента");
-
+*/
 
 //Task 36.
 //Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов,
 //стоящих на нечетных позициях (i=1(3,5,7...))
+
+int[] NewArrayCreate(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+        array[i]=new Random().Next(minValue,maxValue+1);
+    return array;
+}
+
+void ShowArray(int[]array)
+{
+    for(int i=0;i<array.Length; i++)
+        Console.Write(array[i] + " ");
+}Console.WriteLine();
+
+int OddIndexSum(int[]array)
+{
+    int oddSum = 0;
+    for(int i=1;i<array.Length;i=i+2)
+        oddSum++;
+    return oddSum;
+}
+
+Console.WriteLine("Задайте размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте min: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте max: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int [] myarray = NewArrayCreate(length,min,max);
+ShowArray(myarray);
+Console.WriteLine("Sum of numbers with odd index is ")
 
 //Task38.
 //Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным 
