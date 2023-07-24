ecde6a64703b06cc6fd9c6ad76ad0061cf950e28
GrinIv <svetili@bk.ru>
2023-07-25 00:44:31 +0300

    seminar5Task34

--- 5_sem/Program.cs
+++ 5_sem/Program.cs
@@ -1,6 +1,41 @@
 ﻿//Task 34. Задайте массив заполненный случайными трехзначными положительными числами.
 //напишите программу, которая покажет количество четных чисел в массиве
 
+int[] CreateArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+
+void ShowArray(int []array)
+{
+    for (int i = 0; i<array.Length; i++) 
+        Console.Write(array[i]+" ");
+    Console.WriteLine();
+}
+
+int CountEvenNumber(int []array)
+{
+    int count = 0;
+    for(int i=0;i<array.Length;i++)
+    {
+       if(array[i]%2==0)
+            count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Задайте размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int min = 100;
+int max = 999;
+int[] myarray = CreateArray(length,min,max);
+ShowArray(myarray);
+
+Console.WriteLine("Массив содержит " + CountEvenNumber(myarray) + " четных элемента");
+
 
 //Task 36.
 //Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов,
