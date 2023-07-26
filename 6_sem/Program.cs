ad6b6ec76f84efbe134858cd3fafa7359668dc81
GrinIv <svetili@bk.ru>
2023-07-26 20:49:54 +0300

    6 sem HW task43

--- 6_sem/Program.cs
+++ 6_sem/Program.cs
@@ -1,18 +1,47 @@
 ﻿//task41
+/*
 void CountPositiveNumbers()
 {
 Console.WriteLine("Input number of numbers");
 int n = Convert.ToInt32(Console.ReadLine());
 int count=0;
-    for(int i=1;i<n;i++)
+    for(int i=1;i<=n;i++)
         {
-            console.WriteLine("Input a number: ");
-            int number=Console.ReadLine();
+            Console.WriteLine("Input a "+i+" number: ");
+            int number=Convert.ToInt32(Console.ReadLine());
             if(number>0) 
             count++;
         }
-    Console.WriteLine("Number of positive numbers is: "+ count);
+    Console.WriteLine("You have entered: "+ count + " positive numbers");
 
 }
 CountPositiveNumbers();
+
+*/
 //task43
+
+
+void PointOfIntersection()
+{
+Console.WriteLine("Input ratio 'K' for the first equation: ");
+double k=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input ratio 'K' for the second equation: ");
+double k2=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input ratio 'b' for the first equation: ");
+double b=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input ratio 'b' for the second equation: ");
+double b2=Convert.ToDouble(Console.ReadLine());
+
+    if(k==k2)
+        if(b==b2)
+        Console.WriteLine("The lines match");
+        else
+        Console.WriteLine("The lines are parallel");
+    else
+    {
+    double x = (b2-b)/(k-k2);
+    double y = k*x+b;
+    Console.WriteLine($"Coordinates of intersection point: ({x}, {y})");
+    }
+}
+PointOfIntersection();
