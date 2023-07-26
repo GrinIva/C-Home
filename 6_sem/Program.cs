8242e81bf1f4fcf7befbbfa5472b4e8949b837c5
GrinIv <svetili@bk.ru>
2023-07-26 19:13:31 +0300

    6 sem HW task41

---
+++ 6_sem/Program.cs
@@ -0,0 +1,18 @@
+﻿//task41
+void CountPositiveNumbers()
+{
+Console.WriteLine("Input number of numbers");
+int n = Convert.ToInt32(Console.ReadLine());
+int count=0;
+    for(int i=1;i<n;i++)
+        {
+            console.WriteLine("Input a number: ");
+            int number=Console.ReadLine();
+            if(number>0) 
+            count++;
+        }
+    Console.WriteLine("Number of positive numbers is: "+ count);
+
+}
+CountPositiveNumbers();
+//task43
