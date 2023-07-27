78cd35b6ac81cbdc3d07e494bc01ce44bb1f6869
GrinIv <svetili@bk.ru>
2023-07-27 22:30:55 +0300

    52 task

--- 7_sem/Program.cs
+++ 7_sem/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 //47 task
+//Задайте двумерный массив заполненный случайными вещественными числами
 /*
 void PrintArray(double[,] matr) // Метод вывода массива на экран
 {
@@ -35,6 +36,9 @@ PrintArray(matrix);
 
 
 //50 task
+//Напишите программу, которая на входе принимает позиции элемента в двумерном массиве
+//и возхвращает его значение. Или же возвращает указание, что такого элемента не существует
+/*
 void FillArray(int[,] matr) //метод заполнения массива рандомными значениями
 {
     for (int i = 0; i < matr.GetLength(0); i++)
@@ -71,6 +75,7 @@ bool ChekIndex(int[,] matr, int str, int col)
 int[,] matrix = new int[new Random().Next(1,10), new Random().Next(1,10)]; // Игнициализация двумерного массива
 FillArray(matrix);
 PrintArray(matrix);
+
 Console.WriteLine("inp i");
 int str = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("inp j");
@@ -80,3 +85,63 @@ if (ChekIndex(matrix,str,col)==true)
     Console.WriteLine("Your number is " + matrix[str,col]);
 else
     Console.WriteLine("No such element");
+*/
+
+
+//52 task
+//задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов 
+//каждого столбца
+
+//int метод заполнения двумерного массива
+int[,] Create2dArray(int row, int col, int minValue, int maxValue) 
+{
+    int[,] array = new int[row, col];
+
+    for(int i=0;i<row;i++)
+        for(int j=0;j<col;j++)
+            array[i,j] = new Random().Next(minValue,maxValue);
+    return array;
+}
+// Метод вывода массива на экран
+void PrintArray(int[,] matr) 
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+
+            Console.Write($"{matr[i, j]} ");
+
+        Console.WriteLine();
+    }
+}
+
+void MeanNumber(int[,] matr) // Метод вывода массива на экран
+{
+    int mean = 0
+
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        for (int i = 0; i < matr.GetLength(0); i++)
+
+            mean += matr[i,j];
+
+
+        Console.WriteLine("Arithmetic mean of numbers in " + j +" colom is "+ mean);
+    }
+}
+
+
+Console.WriteLine("Задайте количество строк массива: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов массива: ");
+int col = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте минимальное значение элемента массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте максимальное значение элемента массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+
+int[,]myarray = Create2dArray(row,col,min,max);
+PrintArray(myarray);
+MeanNumber(myarray);
