def80a5d4597b48afd557712faefe5274af3ff97
GrinIv <svetili@bk.ru>
2023-08-08 21:28:08 +0300

    sem8 task62

--- 8_sem/Program.cs
+++ 8_sem/Program.cs
@@ -293,5 +293,52 @@ Show3dArray(my3darray);
 
 */
 
-//
+//Task62
+//Напишите программу, которая заполнит спирально массив 4 на 4
 
+int[,]Create2DArray(int row, int col)
+{
+
+    
+    int[,] array = new int[row, col];
+ 
+    int temp = 1;
+    int i = 0;
+    int j = 0;
+ 
+    while (temp <= array.GetLength(0) * array.GetLength(1))
+    {
+    array[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < array.GetLength(1) - 1)
+        j++;
+    else if (i < j && i + j >= array.GetLength(0) - 1)
+        i++;
+    else if (i >= j && i + j > array.GetLength(1) - 1)
+        j--;
+    else
+        i--;
+    }
+    return array;
+}
+
+void Show2dArray(int [,]array)
+{
+
+    for(int i =0;i<array.GetLength(0);i++)
+    {
+        for(int j=0;j<array.GetLength(1);j++)
+            Console.Write(array[i,j]+" ");
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("input row: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input col: ");
+int col = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] my2darray = Create2DArray(row,col);
+Show2dArray(my2darray);
+Console.WriteLine();
